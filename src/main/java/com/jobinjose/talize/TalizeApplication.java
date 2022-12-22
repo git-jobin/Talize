181b664fe8ca39b0a7f9bb0947dbcd325819de54
Jobin Jose <mail.jobinjose.in>
2022-12-22 07:47:15 +0530

    Initial Commit

---
+++ src/main/java/com/jobinjose/talize/TalizeApplication.java
@@ -0,0 +1,16 @@
+package com.jobinjose.talize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class TalizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalizeApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/jobinjose/talize/TalizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobinjose.talize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
